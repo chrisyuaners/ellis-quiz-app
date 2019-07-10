f572b56fb471f7b6a488755ba7d196cc587db01e
chrisyuaners <chrisyuaners@gmail.com>
2019-07-10 09:17:05 -0400

    added chart

--- project-backend/db/schema.rb
+++ project-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_140357) do
+ActiveRecord::Schema.define(version: 2019_07_09_205225) do
 
   create_table "card_choices", force: :cascade do |t|
     t.integer "card_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_140357) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "choices", force: :cascade do |t|
